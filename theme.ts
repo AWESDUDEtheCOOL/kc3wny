401ca77d01d3520c110b3fada458a8c5d80f58bd
Mason Matich <awesdudethecool@gmail.com>
2025-01-02 20:22:25 -0500

    vercel npm compile fix 4

--- theme.ts
+++ theme.ts
@@ -1,8 +1,8 @@
 'use client';
 
+import { createTheme } from '@mantine/core';
 import { Workbench } from 'next/font/google';
 import localFont from 'next/font/local';
-import { createTheme } from '@mantine/core';
 
 // Google fonts
 export const workbench = Workbench({
@@ -45,5 +45,5 @@ export const theme = createTheme({
       '#000000', // Darkest
     ],
   },
-  primaryColor: 'dark', // Use the custom dark color
+  primaryColor: 'dark',
 });
