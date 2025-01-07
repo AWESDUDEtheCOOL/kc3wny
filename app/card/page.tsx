7e068585cb86ab879aa455748de5704aef438299
Mason Matich <awesdudethecool@gmail.com>
2025-01-07 13:54:00 -0800

    card page

---
+++ app/card/page.tsx
@@ -0,0 +1,26 @@
+import { Text } from '@mantine/core';
+import { Footer } from '../../components/Footer/Footer';
+import { Header } from '../../components/Header/Header';
+
+export default function AboutPage() {
+  return (
+    <>
+      <Header />
+      <Text
+        c="dimmed"
+        ta="center"
+        size="lg"
+        maw={580}
+        mx="auto"
+        mt="xl"
+        style={{
+          fontFamily: 'ppEditorialNew',
+          fontVariationSettings: '"wght" 200',
+        }}
+      >
+        This is my card
+      </Text>
+      <Footer />
+    </>
+  );
+}
